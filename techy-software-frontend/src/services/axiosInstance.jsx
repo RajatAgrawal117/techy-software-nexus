c1a8895c764593694d93ad434fcc03eff588ba52
Rajat Agrawal <rajat.ace22@sot.pdpu.ac.in>
2025-07-31 15:09:21 +0530

    fix: Update axios baseURL to use environment variable

--- techy-software-frontend/src/services/axiosInstance.jsx
+++ techy-software-frontend/src/services/axiosInstance.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Attach JWT token to each request if available
